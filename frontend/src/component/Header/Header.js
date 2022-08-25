53ad87ae40bbf77a4f13533425f1d3e189b2863b
anandu <ananduvijayan8@gmail.com>
2022-08-25 12:17:50 +0530

    Login working done

--- frontend/src/component/Header/Header.js
+++ frontend/src/component/Header/Header.js
@@ -1,33 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
+import {NavLink, useNavigate } from 'react-router-dom';
+import { NavBtnLink } from 'react';
 
 
+const Navigate = useNavigate
 const Header = () => {
+  const [user,setUser]=useState('')
+  useEffect(() => {
+    const userInfo = localStorage.getItem('userInfo')
+    if(userInfo){
+      setUser(JSON.parse(userInfo))
+      console.log(userInfo);
+      
+    }
+  },[])
+  const logout=()=>{
+    localStorage.removeItem("userInfo")
+    Navigate('/')
+  }
+
+
   return (
     <div>
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="#home">Login</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-          </Nav>
-          <Nav>
-           
-                <a href='/login'>
-                  <Button >Login</Button>
-                  </a>
-                  <a href='/signup'>
-                  <Button>Signup</Button>
-                  </a>
-                
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Login</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+            </Nav> 
+            
+              {user ? <NavLink onClick={logout} to='/'>
+                Logout
+              </NavLink> : <NavLink to="/login">
+                Login
+              </NavLink>
+              }
+          
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   )
 }
